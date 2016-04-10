82ee243e6a002d53fcba92a121c96203fa89c2fe
Craig J Perry <craigp84@gmail.com>
2016-04-10 13:45:17 +0100

    chore(deps): Add mocha

---
+++ test/index.js
@@ -0,0 +1,13 @@
+let http = require('http');
+let assert = require('assert');
+
+require('../lib/index.js');
+
+describe('Example Node Server', () => {
+  it('should return 200', done => {
+    http.get('http://127.0.0.1:3000', res => {
+      assert.equal(200, res.statusCode);
+      done();
+    });
+  });
+});
