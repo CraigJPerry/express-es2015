0eea5559dc711e603214548f84407fbbd81b1f61
Craig J Perry <craigp84@gmail.com>
2016-04-10 21:57:51 +0100

    test(things): Describe test cases for CRUDing Things

--- test/index.js
+++ test/index.js
@@ -11,3 +11,13 @@ describe('Example Node Server', () => {
     });
   });
 });
+
+describe('CRUD Things', () => {
+  it('should create new things');
+  it('should refuse to create existing things');
+  it('should list all things');
+  it('should filter the list of things');
+  it('should update an existing thing');
+  it('should refuse to update a non-existing thing');
+  it('should delete the chosen thing');
+});
